c46c801a2ca00eedfb123a4979bf07a558ce15ac
Vincent Kreuzer <vincent.kreuzer@collatum-it.de>
2022-08-19 14:45:57 +0200

    initial commit

---
+++ src/main/java/com/collatum/ipfhaachen/IpFhAachenApplication.java
@@ -0,0 +1,13 @@
+package com.collatum.ipfhaachen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpFhAachenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpFhAachenApplication.class, args);
+    }
+
+}
