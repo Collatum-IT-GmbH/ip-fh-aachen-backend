3dd06ded34ac8cb144482755dec3298e005ec71b
Yannick Bohrer <yannick.bohrer@collatum-it.de>
2022-09-02 14:11:22 +0200

    added cross origin for localhost:4200

--- src/main/java/com/collatum/ipfhaachen/api/PersonController.java
+++ src/main/java/com/collatum/ipfhaachen/api/PersonController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.collatum.ipfhaachen.business.service.PersonService;
+
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins="localhost:4200")
 public class PersonController {
 
     private final @NonNull PersonService personService;
